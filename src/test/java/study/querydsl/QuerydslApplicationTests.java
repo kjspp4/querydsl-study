26d62250163a4aefba6d7cf02aad45297519b236
김종민 <kjspp4@gmail.com>
2020-10-08 17:48:15 +0900

    최초커밋

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,16 @@
+package study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+}
